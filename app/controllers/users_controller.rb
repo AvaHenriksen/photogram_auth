e52892b2257a05eaa7faf9158dd491dc6e5538f5
AvaHenriksen <ava.henriksen@gmail.com>
2017-02-17 14:24:58 -0500

    Commit 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,23 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+    @photos = @user.photos
+
+
+    if current_user !=nil
+    render("show")
+    else redirect_to("index")
+    end
+  end
+
+  def my_likes
+    @liked_photos = current_user.liked_photos
+    if current_user !=nil
+    render("my_likes")
+    else redirect_to("index")
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :username, :presence => true, :uniqueness => true
+  validates :email, :presence => true, :uniqueness => true
+  validates :password, :presence => true
 
   has_many :photos
+  has_many :likes
   has_many :liked_photos, :through => :likes, :source => :photo
 end
